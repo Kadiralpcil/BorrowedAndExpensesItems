5c3474d23e8fd4862d6c0433390a98698241e32c
Kadiralpcil <kadiralpcil@gmail.com>
2022-04-28 19:25:47 +0300

    deneme
    deneme

---
+++ inAndOut/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace inAndOut.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Borrower { get; set; }
+        public string Lender { get; set; }
+        [DisplayName("Item Name")]
+        public string ItemName { get; set; }
+
+    }
+}
+ 
